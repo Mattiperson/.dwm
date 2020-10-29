648362d69a1a75c160ae6d62deac21d85b6907d1
Mattiperson <71823113+Mattiperson@users.noreply.github.com>
2020-10-30 00:51:12 +0800

    Update because yes

--- config.def.h
+++ config.def.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3.5;        /* border pixel of windows */
-static const unsigned int gappx     = 7;        /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "UbuntuMono Nerd Font:size=14" };
-static const char dmenufont[]       = "UbuntuMono Nerd Font:size=14";
+static const char *fonts[]          = { "TerminessTTF Nerd Font Bold:size=14" };
+static const char dmenufont[]       = "TerminessTTF Nerd Font Bold:size=14";
 static const char col_gray1[]       = "#2E3440"; /* Bar background */
 static const char col_gray2[]       = "#3b4252"; /* Inactive window's border */
 static const char col_gray3[]       = "#D8DEE9"; /* Font */
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { " ", " ", " ", "爵 ", "拾 ", " ", " ", " ", " " };
+static const char *tags[] = { " ", " ", " "," " };
 
 static const Rule rules[] = {
 	/* xprop(1):
--- config.h
+++ config.h
@@ -1,15 +1,15 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3.5;        /* border pixel of windows */
-static const unsigned int gappx     = 7;        /* gaps between windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "UbuntuMono Nerd Font:size=14" };
-static const char dmenufont[]       = "UbuntuMono Nerd Font:size=14";
+static const char *fonts[]          = { "TerminessTTF Nerd Font:size=14" };
+static const char dmenufont[]       = "TerminessTTF Nerd Font:size=14";
 static const char col_gray1[]       = "#2E3440"; /* Bar background */
-static const char col_gray2[]       = "#3b4252"; /* Inactive window's border */
+static const char col_gray2[]       = "#3B4252"; /* Inactive window's border */
 static const char col_gray3[]       = "#D8DEE9"; /* Font */
 static const char col_gray4[]       = "#D8DEE9"; /* Current tag and current window */
 static const char col_cyan[]        = "#5E81AC"; /* Bar highlight */
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { " ", " ", " ", "爵 ", "拾 ", " ", " ", " ", " " };
+static const char *tags[] = { " ", " ", " "," " };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -28,6 +28,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Discord",  NULL,	  NULL,	      0,	    1,           -1 },
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
@@ -61,8 +62,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-// static const char *termcmd[]  = { "st", NULL };
-static const char *termcmd[] = {"alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
+// static const char *termcmd[] = {"alacritty", NULL };
 
 #include"shiftview.c"
 static Key keys[] = {
@@ -112,11 +113,11 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
 	{ MODKEY|ControlMask,           XK_l,      spawn,          SHCMD("betterlockscreen --lock dimblur")},
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("scrot") },
+	{ MODKEY,	                XK_s,      spawn,          SHCMD("flameshot full -p ~/images/screenshots") },
 
 /* Apps */
-	{ MODKEY, XK_r, spawn, SHCMD("redshift -O 2400") },
-        { MODKEY|ShiftMask, XK_r, spawn, SHCMD("redshift -x") },
+	{ MODKEY,			XK_r,	   spawn, 	   SHCMD("redshift -O 2400") },
+        { MODKEY|ShiftMask, 		XK_r,	   spawn,	   SHCMD("redshift -x") },
 /* Hardware */
 	{ MODKEY,                       XK_Up,     spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%")},	
 	{ MODKEY,                       XK_Down,   spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%")},
